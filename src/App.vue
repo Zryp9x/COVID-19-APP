a2b8dbacc1cd2584ab10712ee3d0c8e1b3139653
Czeksu <camiloczekaj@gmail.com>
2020-04-24 23:46:41 +0200

    Update App.vue

--- src/App.vue
+++ src/App.vue
@@ -5,9 +5,9 @@
           <div class="collapse navbar-collapse" id="navbarNavAltMarkup">
               <div class="navbar-nav">
                   <router-link to="/COVID-19-APP/">Home</router-link>
-                  <router-link to="/COVID-19-APP/countries">Państwa</router-link>
-                  <router-link to="/COVID-19-APP/info">Informacje</router-link>
-                  <router-link to="/COVID-19-APP/authors">Authors</router-link>
+                  <router-link to="/countries">Państwa</router-link>
+                  <router-link to="/info">Informacje</router-link>
+                  <router-link to="/authors">Authors</router-link>
               </div>
           </div>
       </nav>
