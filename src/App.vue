f198f15c01fb30b2b6b0a41f2358e4d9a6715e64
Czeksu <camiloczekaj@gmail.com>
2020-04-25 01:49:34 +0200

    Update App.vue

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,7 @@
 <template>
   <div class="container">
     <nav class="navbar">
-        <img class="img" @click="$router.push('/')" src="https://www.petfinder.com/wp-content/uploads/2012/11/91615172-find-a-lump-on-cats-skin-632x475.jpg"><br><br>
+        <img class="img" @click="$router.push('/')" src="https://www.petfinder.com/wp-content/uploads/2012/11/91615172-find-a-lump-on-cats-skin-632x475.jpgnpm"><br><br>
         
         <button @click="$router.push('/countries')" type="button" class="btn btn-outline-dark">Państwa</button>
         
