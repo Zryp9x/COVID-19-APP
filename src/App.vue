5031e36d18eb80c2db351d05529cae5a06280f58
Czeksu <camiloczekaj@gmail.com>
2020-04-26 18:31:34 +0200

    Update App.vue

--- src/App.vue
+++ src/App.vue
@@ -2,11 +2,8 @@
   <div class="container">
     <nav class="navbar">
         <img class="img" @click="$router.push('/').catch(err =>{})" src="https://img.icons8.com/nolan/64/coronavirus.png"><br><br>
-        
-        <button @click="$router.push('/countries').catch(err =>{})" type="button" class="btn btn-outline-dark">Państwa</button>
-        
-        <button  @click="$router.push('/info').catch(err =>{})" type="button" class="btn btn-outline-dark">Informacje</button>
-        
+        <button @click="$router.push('/countries').catch(err =>{})" type="button" class="btn btn-outline-dark">Countries</button>
+        <button  @click="$router.push('/info').catch(err =>{})" type="button" class="btn btn-outline-dark">Info</button>
         <button  @click="$router.push('/authors').catch(err =>{})" type="button" class="btn btn-outline-dark">Authors</button>
         <br><br>
     </nav>
