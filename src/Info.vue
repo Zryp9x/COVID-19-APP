f1558a0484317de07c97e07881864cb88ed96c60
Czeksu <camiloczekaj@gmail.com>
2020-04-28 15:53:14 +0200

    Update Info.vue

--- src/Info.vue
+++ src/Info.vue
@@ -1,3 +1,54 @@
 <template>
-    <h1>INFORMACJE</h1>
+    <div>
+        <h1>General informations about COVID-19</h1>
+        <mdb-carousel :interval="6000" controlls indicators slide :items="items"></mdb-carousel>
+        <h4>Wash your hands frequently</h4>
+        <p>Regular washing your hands with soap and water or using alcohol-based hand rub kills viruses that may be on your hands.</p>
+    </div>
 </template>
+<script>
+  import { mdbCarousel } from "mdbvue";
+  export default {
+    name: "CarouselPage",
+    components: {
+      mdbCarousel
+    },
+    data() {
+      return {
+        items: [
+          {
+            img: true,
+            src:"https://www.who.int/images/default-source/health-topics/coronavirus/risk-communications/general-public/safe-greetings.png?sfvrsn=2e97004e_2"
+          },
+          {
+            img: true,
+            src:"https://www.who.int/images/default-source/health-topics/coronavirus/risk-communications/general-public/handshaking.png?sfvrsn=4aed53c5_2"
+          },
+          {
+            img: true,
+            src:"https://www.who.int/images/default-source/health-topics/coronavirus/risk-communications/general-public/wearing-gloves.png?sfvrsn=ec69b46a_2"
+          },
+          {
+            img: true,
+            src:"https://www.who.int/images/default-source/health-topics/coronavirus/eng-mythbusting-ncov-(19).tmb-1920v.png"
+          },
+          {
+            img: true,
+            src:"https://www.who.int/images/default-source/health-topics/coronavirus/eng-mythbusting-ncov-(13).tmb-1920v.png"
+          },
+          {
+            img: true,
+            src:"https://www.who.int/images/default-source/health-topics/coronavirus/eng-mythbusting-ncov-(23).tmb-1920v.png"
+          },
+          {
+            img: true,
+            src:"https://www.who.int/images/default-source/health-topics/coronavirus/eng-mythbusting-ncov-(30).tmb-1920v.png"
+          },{
+            img: true,
+            src:"https://www.who.int/images/default-source/health-topics/coronavirus/eng-mythbusting-ncov-(33).tmb-1920v.png"
+          }
+        ]
+      };
+    }
+  };
+</script>
