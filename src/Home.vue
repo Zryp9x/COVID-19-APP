b838d8baf3327b8054f02240687aa32fdd79aec7
Zryp9x <sneefeeh@gmail.com>
2020-04-28 20:29:58 +0200

    Update Home.vue

--- src/Home.vue
+++ src/Home.vue
@@ -10,11 +10,11 @@
           </div>
           <div class="col-md-6">
               <h4 class="dataText">New confirmed:</h4>
-              <p class="data">{{countries.Global.NewConfirmed}} <img class="img" src="https://www.zovirax.pl/images/el1.png" width="40px" height="35px" style="margin-bottom:10px;"></p>
+              <p class="data">{{countries.Global.NewConfirmed}} <img class="img" src="https://upload.wikimedia.org/wikipedia/commons/thumb/f/fe/Green-Up-Arrow.svg/807px-Green-Up-Arrow.svg.png" width="40px" height="35px" style="margin-bottom:10px;"></p>
               <h4 class="dataText">New deaths:</h4>
-              <p class="data">{{countries.Global.NewDeaths}} <img class="img" src="https://pngimage.net/wp-content/uploads/2018/05/christian-cross-png-6.png" width="40px" height="35px" style="margin-bottom:10px;"></p>
+              <p class="data">{{countries.Global.NewDeaths}} <img class="img" src="https://gifyagusi.pl/img/2019/10/24/obrazek-gif/zaloba/Kir%20-%20przezroczysta%20żałobna%20wstążka%20bez%20tła.png" width="40px" height="35px" style="margin-bottom:10px;"></p>
               <h4 class="dataText">New recovered:</h4>
-              <p class="data">{{countries.Global.NewRecovered}} <img class="img" src="https://data3.cupsell.pl/upload/generator/183511/640x420/2915072_print_1.png?resize=max_sizes&key=55f9a22768eed085006592c1174c0235" width="40px" height="40px" style="margin-bottom:10px;"></p>
+              <p class="data">{{countries.Global.NewRecovered}} <img class="img" src="https://upload.wikimedia.org/wikipedia/commons/thumb/a/a3/Recycling_symbol2.svg/1024px-Recycling_symbol2.svg.png" width="40px" height="40px" style="margin-bottom:10px;"></p>
           </div>
       </div>
 </template>
