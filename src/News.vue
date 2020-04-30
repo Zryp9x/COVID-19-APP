4e9c8b4cf20ec6f8032aa68ec6e745a999ce9f79
Czeksu <camiloczekaj@gmail.com>
2020-04-30 21:13:12 +0200

    Update News.vue

--- src/News.vue
+++ src/News.vue
@@ -1,7 +1,7 @@
 <template>
         <div class="row">
           <div class="border border-danger rounded col-md-6">
-            <h2 style="text-align: center">CDC Online Newsroom</h2>
+            <h2 style="text-align: center"><img style="width:70px; height:50px; align-item: center" src="https://upload.wikimedia.org/wikipedia/commons/4/45/US-CDC-Logo.png">CDC Online Newsroom</h2>
             <div style="max-height: 340px; overflow-y: scroll;" class="pre-scrollable border border-danger rounded">
               <div v-for="(item, index) in news.rss.channel[0].item" :key=index>  
                   <h4 style="text-align: center">{{news.rss.channel[0].item[index].title[0]}}</h4>
@@ -12,10 +12,11 @@
             </div>
           </div>
           <div class="border border-danger rounded col-md-6">
-            <h2 style="text-align: center">Twitter updates</h2>
+            <h2 style="text-align: center"><img style="width:70px; height:50px; align-item: center" src="https://upload.wikimedia.org/wikipedia/commons/2/2f/Flag_of_the_United_Nations.svg">UN News</h2>
             <div style="max-height: 340px; overflow-y: scroll;" class="pre-scrollable border border-danger rounded">
               <div v-for="(item, index) in ednews.rss.channel[0].item" :key=index>  
                   <h4 style="text-align: center">{{ednews.rss.channel[0].item[index].title[0]}}</h4>
+                  <p id="description" style="text-align: left">{{ednews.rss.channel[0].item[index].description[0]}}</p>
                   <p type="url" style="text-align: center">{{ednews.rss.channel[0].item[index].pubDate[0]}}</p>
                   <hr color="red" size="10">
               </div>
@@ -42,7 +43,7 @@ export default {
             self.news = result
             });        
         })
-         axios.get('https://twitrss.me/twitter_user_to_rss/?user=AlrtCoronaVirus')
+         axios.get('https://news.un.org/feed/subscribe/en/news/topic/health/feed/rss.xml')
         .then(response => {
             var t = this; 
             parseString(response.data, function (err, result) {
