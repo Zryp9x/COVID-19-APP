8bf3414238b30ee5c7045e8887628d1a2e518db0
Zryp9x <sneefeeh@gmail.com>
2020-05-01 15:15:48 +0200

    Update navbar

--- src/App.vue
+++ src/App.vue
@@ -8,25 +8,25 @@
         <label class="menu-icon" for="menu-btn"><span class="navicon"></span></label>
         <ul class="menu"> 
           <li> 
-            <a @click="$router.push('/countries').catch(err =>{})" style="margin-top: 15px; margin-right: 5px;" v-if="this.$route.path !== '/' && this.$route.path !== '/register'">Countries</a>
+            <a @click="$router.push('/countries').catch(err =>{})" v-if="this.$route.path !== '/' && this.$route.path !== '/register'">Countries</a>
           </li>
           <li> 
-            <a @click="$router.push('/info').catch(err =>{})" style="margin-top: 15px; margin-right: 5px;" v-if="this.$route.path !== '/' && this.$route.path !== '/register'">Info</a>
+            <a @click="$router.push('/info').catch(err =>{})" v-if="this.$route.path !== '/' && this.$route.path !== '/register'">Info</a>
           </li>
           <li> 
-            <a @click="$router.push('/news').catch(err =>{})" style="margin-top: 15px; margin-right: 5px;" v-if="this.$route.path !== '/' && this.$route.path !== '/register'">News</a>
+            <a @click="$router.push('/news').catch(err =>{})" v-if="this.$route.path !== '/' && this.$route.path !== '/register'">News</a>
           </li>
           <li> 
-            <a @click="$router.push('/authors').catch(err =>{})" style="margin-top: 15px; margin-right: 5px;" v-if="this.$route.path !== '/' && this.$route.path !== '/register'">Authors</a>
+            <a @click="$router.push('/authors').catch(err =>{})" v-if="this.$route.path !== '/' && this.$route.path !== '/register'">Authors</a>
           </li>
           <li> 
-            <a @click="$router.push('/').catch(err =>{})" style="margin-top: 15px; margin-right: 5px;" v-if="this.$route.path !== '/countries' && this.$route.path !== '/info' && this.$route.path !== '/countryinfo' && this.$route.path !== '/news' && this.$route.path !== '/authors' && this.$route.path !== '/home'">Login</a>
+            <a @click="$router.push('/').catch(err =>{})" v-if="this.$route.path !== '/countries' && this.$route.path !== '/info' && this.$route.path !== '/countryinfo' && this.$route.path !== '/news' && this.$route.path !== '/authors' && this.$route.path !== '/home'">Login</a>
           </li>
           <li> 
-            <a @click="$router.push('/register').catch(err =>{})" style="margin-top: 15px; margin-right: 5px; border-right: 0;" v-if="this.$route.path !== '/countries' && this.$route.path !== '/info' && this.$route.path !== '/countryinfo' && this.$route.path !== '/news' && this.$route.path !== '/authors' && this.$route.path !== '/home'">Register</a>
+            <a @click="$router.push('/register').catch(err =>{})" style="border-right: 0;" v-if="this.$route.path !== '/countries' && this.$route.path !== '/info' && this.$route.path !== '/countryinfo' && this.$route.path !== '/news' && this.$route.path !== '/authors' && this.$route.path !== '/home'">Register</a>
           </li>
           <li> 
-            <a @click="$router.push('/').catch(err =>{})" style="margin-top: 15px; margin-right: 5px; border-right: 0;" v-if="this.$route.path !== '/' && this.$route.path !== '/register'"><logout></logout></a>
+            <a @click="$router.push('/').catch(err =>{})" style="border-right: 0;" v-if="this.$route.path !== '/' && this.$route.path !== '/register'"><logout></logout></a>
           </li>
         </ul>
         </header>
