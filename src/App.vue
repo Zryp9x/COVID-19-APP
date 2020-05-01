7bd9214d8e25ff5c7e2f7570c6a00bdb0ae3a445
Zryp9x <sneefeeh@gmail.com>
2020-05-01 13:56:21 +0200

    Update App.vue

--- src/App.vue
+++ src/App.vue
@@ -2,10 +2,10 @@
   <div class="container">
     <nav class="navbar-brand" style="width: 100%;">
         <img class="img" @click="$router.push('/home').catch(err =>{})" src="https://img.icons8.com/nolan/64/coronavirus.png" style="margin-right:10px;">
-        <button @click="$router.push('/countries').catch(err =>{})" type="button" class="btn btn-outline-dark" v-if="this.$route.path !== '/' && this.$route.path !== '/register'">Countries</button>
-        <button @click="$router.push('/info').catch(err =>{})" type="button" class="btn btn-outline-dark" v-if="this.$route.path !== '/' && this.$route.path !== '/register'">Info</button>
-        <button @click="$router.push('/news').catch(err =>{})" type="button" class="btn btn-outline-dark" v-if="this.$route.path !== '/' && this.$route.path !== '/register'">News</button>
-        <button @click="$router.push('/authors').catch(err =>{})" type="button" class="btn btn-outline-dark" v-if="this.$route.path !== '/' && this.$route.path !== '/register'">Authors</button>
+        <button @click="$router.push('/countries').catch(err =>{})" type="button" class="btn btn-outline-dark" v-if="this.$route.path !== '/' && this.$route.path !== '/register' && this.$route.path !== '/countries'">Countries</button>
+        <button @click="$router.push('/info').catch(err =>{})" type="button" class="btn btn-outline-dark" v-if="this.$route.path !== '/' && this.$route.path !== '/register' && this.$route.path !== '/info'">Info</button>
+        <button @click="$router.push('/news').catch(err =>{})" type="button" class="btn btn-outline-dark" v-if="this.$route.path !== '/' && this.$route.path !== '/register' && this.$route.path !== '/news'">News</button>
+        <button @click="$router.push('/authors').catch(err =>{})" type="button" class="btn btn-outline-dark" v-if="this.$route.path !== '/' && this.$route.path !== '/register' && this.$route.path !== '/authors'">Authors</button>
         <button @click="$router.push('/').catch(err =>{})" type="button" class="btn btn-outline-dark" v-if="this.$route.path !== '/countries' && this.$route.path !== '/info' && this.$route.path !== '/countryinfo' && this.$route.path !== '/news' && this.$route.path !== '/authors' && this.$route.path !== '/home'">Login</button>
         <button @click="$router.push('/register').catch(err =>{})" type="button" class="btn btn-outline-dark" v-if="this.$route.path !== '/countries' && this.$route.path !== '/info' && this.$route.path !== '/countryinfo' && this.$route.path !== '/news' && this.$route.path !== '/authors' && this.$route.path !== '/home'">Register</button>
         <button @click="$router.push('/').catch(err =>{})" type="button" class="btn btn-outline-dark" v-if="this.$route.path !== '/' && this.$route.path !== '/register'" style="float:right;margin-top: 10px;"><logout></logout></button>
