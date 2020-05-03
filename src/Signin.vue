c30ba8868468e440f9b8b75c2b55b0091b8b2a77
Zryp9x <sneefeeh@gmail.com>
2020-05-03 21:05:48 +0200

    Update Signin.vue

--- src/Signin.vue
+++ src/Signin.vue
@@ -1,7 +1,10 @@
 <template>
     <div class="row" style="margin:auto; width: 300px;">
             <form @submit.prevent="pressed">
-                <h3 class="display-4" style="text-align:center;">Sign in</h3><br>
+                <h1 class="display-4" style="text-align:center; font-size: 45px;">COVID 19 APP</h1>
+                <h1 class="display-4" style="text-align:center; font-size: 30px;">Coronavirus info in one place</h1>
+                <hr>
+                <h1 class="display-4" style="text-align:center;">Sign in</h1><br>
                 <div class="form-group">
                 <label for="exampleInputEmail1">Email address</label>
                 <input type="email" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" placeholder="Enter email" v-model="email"  style="width: 300px;">
@@ -11,6 +14,9 @@
             <input type="password" class="form-control" id="exampleInputPassword1" placeholder="Password" v-model="password" />
                  </div>
             <button type="submit" class="btn btn-primary">Submit</button><br><br>
+            <div style="text-align:center;">
+            <button type="button" class="btn btn-default" @click="signWithGoogle"><img src="https://i2.wp.com/admonkey.pl/wp-content/uploads/2016/12/google-logo-png.png" width="50px" height="50px"></button>
+            </div>
             <div class="error" v-if="error">{{error.message}}</div>
             </form>
             
@@ -42,7 +48,28 @@ export default {
         .catch(error => {
           this.error = error;
         });
+    },
+    signWithGoogle(){
+      var provider = new firebase.auth.GoogleAuthProvider();
+      firebase.auth().signInWithRedirect(provider);
+        firebase.auth().getRedirectResult().then(function(result) {
+        if (result.credential) {
+          // This gives you a Google Access Token. You can use it to access the Google API.
+          var token = result.credential.accessToken;
+          // ...
+        }
+        // The signed-in user info.
+        var user = result.user;
+      }).catch(function(error) {
+        // Handle Errors here.
+        var errorCode = error.code;
+        var errorMessage = error.message;
+        // The email of the user's account used.
+        var email = error.email;
+        // The firebase.auth.AuthCredential type that was used.
+        var credential = error.credential;
+        // ...
+      });
     }
-  }
-  };
+  }}
 </script>
